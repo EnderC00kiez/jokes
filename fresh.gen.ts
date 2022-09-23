ec76a03bd9035224290dad12b856e2b363a22c1f
Ike Welborn <49785023+EnderC00kiez@users.noreply.github.com>
2022-09-23 13:04:37 +0000

    tryna fix

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,7 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Joke.tsx";
 
 const manifest = {
@@ -16,7 +16,7 @@ const manifest = {
     "./routes/index.tsx": $2,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Countdown.tsx": $$0,
     "./islands/Joke.tsx": $$1,
   },
   baseUrl: import.meta.url,
