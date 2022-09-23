60a2d17f1c8e8061e226caa552b895fee3277229
Ike Welborn <49785023+EnderC00kiez@users.noreply.github.com>
2022-09-23 15:43:00 +0000

    counter

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,15 @@
-import Counter from "../islands/Counter.tsx";
+import Countdown from "../islands/Countdown.tsx"
 import Joke from "../islands/Joke.tsx";
 
 export default function Home() {
-  const date = new Date();
-  date.setHours(date.getHours() + 1);
-  return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <Joke />
-      <p>
-      <Countdown target={date.toISOString()} />.
-      </p>
-    </div>
+    const date = new Date();
+    date.setHours(date.getHours() + 1);
+    return (
+        <div class="p-4 mx-auto max-w-screen-md">
+          <Joke />
+          <p>
+      Something is happening <Countdown target={date.toISOString()} />.
+          </p>
+        </div>
   );
 }
