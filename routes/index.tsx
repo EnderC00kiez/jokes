6e915d1d856b81e9022f1ce6848740db53e4bf98
Ike Welborn <49785023+EnderC00kiez@users.noreply.github.com>
2022-09-23 12:49:58 +0000

    remove counter and fresh logo

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,13 +4,7 @@ import Joke from "../islands/Joke.tsx";
 export default function Home() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
       <Joke />
-      <Counter start={3} />
     </div>
   );
 }
