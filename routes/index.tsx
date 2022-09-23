ed4f0c2c7dfcc0e959679ab1c7eb73d8c5afdf6b
Ike Welborn <49785023+EnderC00kiez@users.noreply.github.com>
2022-09-23 13:01:38 +0000

    changed

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div class="p-4 mx-auto max-w-screen-md">
       <Joke />
     <p>
-      The big event is happening <Countdown target={date.toISOString()} />.
+      <Countdown target={date.toISOString()} />.
     </p>
     </div>
   );
