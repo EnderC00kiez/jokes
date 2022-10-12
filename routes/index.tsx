ebdfa95143260c2bd1f1ae3e6ac15d2ee8dc3324
Ike Welborn <49785023+EnderC00kiez@users.noreply.github.com>
2022-10-12 15:24:25 +0000

    ⚗️ Title ⚗️

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     const date = new Date();
     date.setHours(date.getHours() + 1);
     return (
+        <title>Jokes</title>
         <div class="p-4 mx-auto max-w-screen-md">
           <Joke />
         </div>
